6356287682c847f83efc63af4c700140dd722eaf
Soowon Jung <flowerneco@gmail.com>
2019-03-15 14:52:44 +0900

    fix model fin urls

--- 07_database/db/db/urls.py
+++ 07_database/db/db/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',name ='list'),
-    path('',name = 'new'),
-    path('',name = 'delete'),
+    path('movies/',include('movie.urls')),
 ]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- 07_database/db/movie/models.py
+++ 07_database/db/movie/models.py
@@ -6,21 +6,19 @@ class Genre(models.Model):
         return f'{self.id}: {self.name}'
 
 class Movie(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=200)
     audience = models.IntegerField()
     poster_url = models.TextField()
     description = models.TextField()
-    genre = models.ForeignKey(Genre,on_delete=models.SET_DEFAULT,default='')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    genre = models.ForeignKey(Genre, default='', on_delete=models.SET_DEFAULT)
+
     def __str__(self):
         return f'{self.id}: {self.title} , {self.genre.name}'
 
 class Score(models.Model):
     content = models.CharField(max_length=80, default='')
     score = models.IntegerField()
-    movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
     def __str__(self):
         return f'{self.id}: {self.movie.title}/ {self.content}/ {self.score}'
