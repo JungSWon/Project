e838f5c9a5fdc4fee66654ae331b26e2a30d73ae
Soowon Jung <flowerneco@gmail.com>
2019-03-22 11:57:26 +0900

    forming

---
+++ 08_SeedData/seeddata/movies/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'movie'
+
+urlpatterns = [
+    path('',views.movie_list,name ='list'),
+    path('<int:movie_id>/',views.movie_detail,name='detail'),
+    path('<int:movie_id>/',views.movie_delete,name = 'movie_del'),
+
+    path('<int:movie_id>/scores/new',views.score_new,name = 'score_new'),
+    path('<int:movie_id>/scores/<int:score_id>/delete',views.score_delete,name = 'score_del'),
+]
